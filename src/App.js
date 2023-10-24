53db9ad803a131c9743d399ed2ad4593de8e6dc6
anthizy <ebojeleanthony01@gmail.com>
2023-10-24 10:49:24 +0100

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Nav className="me-auto">
         <Nav.Link href="#about">About</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#contact">Contact</Nav.Link>
+        <Nav.Link href="#contact">Contacts</Nav.Link>
       </Nav>
     </Container>
   </Navbar>
